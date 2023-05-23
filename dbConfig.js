f6d362e3047094806bc9b6cb4e625727e2e1fb4a
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2023-05-23 10:07:36 +0100

    modified host to hostname inre database connection

--- dbConfig.js
+++ dbConfig.js
@@ -15,7 +15,7 @@ const dotenv = require('dotenv').config();
 
 module.exports = {
     user: process.env.USER,
-    host: process.env.HOST,
+    hostname: process.env.HOSTNAME,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
     port: process.env.PORT
