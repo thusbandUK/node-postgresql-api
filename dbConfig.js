a45b7a354c1977503c846bba8755b9eb1442a37e
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2023-06-15 09:39:10 +0100

    sensitive details removed from dbconfig

--- dbConfig.js
+++ dbConfig.js
@@ -1,16 +1,4 @@
-/* If it is not important to hide login details, ie during development, the database login details can be included as shown */
-/*
-module.exports = {
-    user: 'me',
-    host: 'renderhost',
-    database: 'api',
-    password: 'password',
-    port: 5432,
-};
-*/
 
-/*For production mode, to keep login details out of repo, the below format can be used*/
-/*This obtains login details from .env file */
 const dotenv = require('dotenv').config();
 
 module.exports = {
