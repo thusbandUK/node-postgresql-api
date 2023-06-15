1c5b31fb03c6535bcb88c0d063281d64075c49d6
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2023-06-15 07:46:11 +0100

    configured to log error message

--- queries.js
+++ queries.js
@@ -15,6 +15,7 @@ const pool = new Pool(dbAccess);
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
     if (error) {
+      console.log(error.stack);
       throw error
     }
     response.status(200).json(results.rows)
